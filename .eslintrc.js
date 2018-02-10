a4a06c0ad8a2c21c9e5bc04d77b08c02a60f2b09
shuang6 <319825685@qq.com>
2018-02-10 21:16:22 +0800

    Add eslint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": [
+      "error",
+      { allow: ["log", "warn", "error"] }
+    ]
+  }
+}
